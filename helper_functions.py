8574d6157060b381513e7a4180fb62fe7fe3cd58
riya0920 <97374899+riya0920@users.noreply.github.com>
2023-04-03 20:06:08 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -10,6 +10,33 @@ class_names = np.array(sorted([item.name for item in data_dir.glob("*")]))
 class_names = class_names[:]
 
 
+
+
+import tensorflow as tf
+
+# Create a function to import an image and resize it to be able to be used with our model
+def load_and_prep_image(filename, img_shape=224, scale=True):
+  """
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  (224, 224, 3).
+  Parameters
+  ----------
+  filename (str): string filename of target image
+  img_shape (int): size to resize target image to, default 224
+  scale (bool): whether to scale pixel values to range(0, 1), default True
+  """
+  # Read in the image
+  img = tf.io.read_file(filename)
+  # Decode it into a tensor
+  img = tf.image.decode_jpeg(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img
+  
 def plot_decision_boundary(model, X, y):
   x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
   y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
